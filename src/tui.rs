18e8a59b936b719474194c05150adb6ab80f7c51
lepton <tonihirvonen00@gmail.com>
2024-10-26 09:31:30 +0300

    search block

--- src/tui.rs
+++ src/tui.rs
@@ -71,14 +71,14 @@ pub fn draw(frame: &mut Frame) {
     let status_area_height = if status_text.is_empty() { 2 } else { 3 };
     let status_block = Block::bordered().title("Status");
 
-    // let vertical = Layout::vertical([Length(3), Min(0), Length(status_area_height)]);
-    let vertical = Layout::vertical([Min(0), Length(status_area_height)]);
-    let [main_area, status_area] = vertical.areas(frame.area());
+    let vertical = Layout::vertical([Min(0)]);
+    let [main_area] = vertical.areas(frame.area());
     let horizontal = Layout::horizontal([Fill(1), Fill(2)]);
     let [left_area, right_area] = horizontal.areas(main_area);
 
-    let left_vertical = Layout::vertical([Length(3), Min(0), Length(5)]);
-    let [profile_area, commands_area, search_area] = left_vertical.areas(left_area);
+    let left_vertical =
+        Layout::vertical([Length(3), Min(0), Length(8), Length(status_area_height)]);
+    let [profile_area, commands_area, search_area, status_area] = left_vertical.areas(left_area);
 
     // Selected text highlight
     // Paragraph::new(status_text).block(Block::default())
@@ -99,15 +99,31 @@ pub fn draw(frame: &mut Frame) {
             .border_style(Style::new()),
         commands_area,
     );
+
+    let search_block = Block::bordered().title("Search").border_style(Style::new());
+    let user_search_block = Block::bordered().title("User").border_style(Style::new());
+    let repo_search_block = Block::bordered().title("Repo").border_style(Style::new());
+
+    let [user_search_area, repo_search_area] =
+        Layout::vertical([Constraint::Percentage(50), Constraint::Percentage(50)])
+            .areas(search_block.inner(search_area));
+    frame.render_widget(&search_block, search_area);
+    frame.render_widget(&user_search_block, user_search_area);
+    frame.render_widget(&repo_search_block, repo_search_area);
     frame.render_widget(
-        Block::bordered().title("Search").border_style(Style::new()),
-        search_area,
+        Paragraph::new("Username").block(Block::default()),
+        user_search_block.inner(user_search_area),
     );
-    frame.render_widget(Block::bordered().title("Right"), right_area);
+    frame.render_widget(
+        Paragraph::new("Repo name").block(Block::default()),
+        repo_search_block.inner(repo_search_area),
+    );
 
     frame.render_widget(&status_block, status_area);
     frame.render_widget(
         Paragraph::new(status_text).block(Block::default()),
         status_block.inner(status_area),
     );
+
+    frame.render_widget(Block::bordered().title("Right"), right_area);
 }
