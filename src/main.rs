be850ba3b0d1163f3e9cb69bd7a6af50b5fa8d4f
lepton <tonihirvonen00@gmail.com>
2024-10-30 00:03:37 +0200

    cl args base

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,64 @@ mod api;
 mod git;
 mod tui;
 
+struct CLATypeError;
+
+struct CLArgs {
+    pub command: Option<String>,
+    pub username: Option<String>,
+    pub repo: Option<String>,
+}
+
+impl CLArgs {
+    fn new() -> Self {
+        Self {
+            command: None,
+            username: None,
+            repo: None,
+        }
+    }
+}
+
+fn get_cl_args() -> Result<CLArgs, CLATypeError> {
+    let args: Vec<String> = std::env::args().collect();
+    let cl_args = CLArgs::new();
+
+    for (i, arg) in args.iter().skip(1).enumerate() {
+        match arg.as_str() {
+            "-u" | "--user" => {
+                println!("{arg} found");
+            }
+            "-r" | "--repo" => {
+                println!("{arg} found");
+            }
+            "-h" | "--help" => {
+                println!("Print help");
+            }
+            _ if arg.starts_with("-") => {
+                println!("Unknown option: {arg}");
+            }
+            _ => {
+                println!(
+                    "{}: '{arg}' is not a gierm command. See 'gierm --help'.",
+                    args[0]
+                );
+                return Err(CLATypeError);
+            }
+        }
+    }
+    return Ok(cl_args);
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args_res = get_cl_args();
+    match args_res {
+        Ok(args) => {}
+        Err(e) => {
+            std::process::exit(0);
+        }
+    }
+
     dotenv::dotenv().ok();
     let gh_access_token = std::env::var("GITHUB_ACCESS_TOKEN").expect("Set GITHUB_ACCESS_TOKEN");
     let username: String = "lepton9".to_string(); // TODO: from env or config
